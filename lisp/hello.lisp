bbca52e979b60b3514049a63bdf7b259b7ceb824
Paul Kennedy <paul.kennedy124@gmail.com>
2022-08-19 10:18:34 -0500

    Starting 'Practical Common Lisp'

--- lisp/hello.lisp
+++ lisp/hello.lisp
@@ -1,2 +1,9 @@
 #!/usr/bin/env -S sbcl --script
-(write-line "Hello, World!")
+
+(defun main ()
+    (format t "Hello, world!~%"))
+;    (write-line "Hello, World!")) ; alternative
+
+(sb-ext:save-lisp-and-die "hello"
+:executable t
+:toplevel 'main)
---
+++ lisp/practical_common_lisp.lisp
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S sbcl --script
+
+; left off at "Improving the User Interaction"
+; https://gigamonkeys.com/book/practical-a-simple-database.html
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defvar *db* nil)
+(defun add-record (cd) (push cd *db*))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+(add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+(add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+(add-record (make-cd "Home" "Dixie Chicks" 9 t))
+
+(dump-db)
